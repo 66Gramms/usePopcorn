b033145c6069dfbb91351802510649767e92f352
66Gramms <meszarosricsi01@gmail.com>
2024-05-08 16:12:39 +0200

    Loading text

--- src/App.jsx
+++ src/App.jsx
@@ -56,15 +56,18 @@ const KEY = "a443438";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = "shrek";
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
+      setIsLoading(false);
     }
     fetchMovies();
   }, []);
@@ -78,7 +81,7 @@ export default function App() {
 
       <Main>
         <ListBox>
-          <MovieList movies={movies} />
+          {isLoading ? <Loader /> : <MovieList movies={movies} />}
         </ListBox>
         <ListBox>
           <WatchedSummary watched={watched} />
@@ -87,6 +90,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 function NavBar({ children }) {
